f1d596967458be8b1c41ee47346ff6033dcb30f9
Minh Bui <buileminh342006@gmail.com>
2025-03-21 16:13:16 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Header } from "./components/Header"
 import { Tabs } from "./components/Tabs"
 import { TodoInput } from "./components/TodoInput"
 import { TodoList } from "./components/TodoList"
+import { useState } from 'react'
+
 
 function App() {
     // const todos = [
@@ -11,8 +13,22 @@ function App() {
     //     { input: 'Say hi to gran gran', complete: true },
     // ]
 
-    
+    const [todos, setTodos] = useState([
+        { input: 'Hello! Add your first todo!', complete: true }
+    ])
 
+    function handleAddTodo() {
+
+    }
+
+    function handleEditTodo() {
+
+    }
+
+    function handleDeleteTodo() {
+
+    }
+    
     return (
         <>
             <Header todos={todos} />
